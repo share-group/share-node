5101a1094ac2b440051bc689fa17d451ffd613cd
ruanzhijun <ruan965176201@qq.com>
2017-06-15 01:08:37 +0800

    集成

---
+++ test/utils/secret.js
@@ -0,0 +1,28 @@
+const test = require('ava');
+const { secret } = require('../index');
+
+const exampleString = 'vbu987ftyukgilofYG^di78o89ygytcrs4YD^UF&IG*FCvbh987FUTYUIBIJ';
+
+const exampleBase64Encode = 'dmJ1OTg3ZnR5dWtnaWxvZllHXmRpNzhvODl5Z3l0Y3JzNFlEXlVGJklHKkZDdmJoOTg3RlVUWVVJQklK';
+const exampleMD5Encode = '1edb3ad009f750fd9e693b757dd06c96';
+const exampleSHA1Encode = '20ee92ef95bf56ae954d7c8838247dab51265a39';
+
+test('test secret.md5()', async (t) => {
+  t.is(secret.md5(''), 'd41d8cd98f00b204e9800998ecf8427e');
+  t.is(secret.md5(exampleString), exampleMD5Encode);
+});
+
+test('test secret.sha1()', async (t) => {
+  t.is(secret.sha1(''), 'da39a3ee5e6b4b0d3255bfef95601890afd80709');
+  t.is(secret.sha1(exampleString), exampleSHA1Encode);
+});
+
+test('test secret.base64Encode()', async (t) => {
+  t.is(secret.base64Encode(''), '');
+  t.is(secret.base64Encode(exampleString), exampleBase64Encode);
+});
+
+test('test secret.base64Decode()', async (t) => {
+  t.is(secret.base64Decode(''), '');
+  t.is(secret.base64Decode(exampleBase64Encode), exampleString);
+});
---
+++ test/utils/time.js
@@ -0,0 +1,27 @@
+const test = require('ava');
+const { time } = require('../index');
+
+test('test util.time.nowMillisecond()', async (t) => {
+  t.is(Math.abs(time.nowMillisecond() - new Date().getTime()) <= 1, true);
+});
+
+test('test util.time.now()', async (t) => {
+  const now = parseInt(new Date().getTime() / 1000, 10) || 0;
+  t.is(time.now(), now);
+});
+
+test('test util.time.format()', async (t) => {
+  t.is(time.format(), time.format('yyyy-MM-dd hh:mm:ss'));
+  t.is(time.format('yyyy-MM-dd hh:mm:ss', 1496327490000), '2017-06-01 22:31:30');
+  t.is(time.format('yyyy-MM-dd', 1496327490000), '2017-06-01');
+  t.is(time.format('yyyyMMdd', 1496327490000), '20170601');
+  t.is(time.format('yyyy', 1496327490000), '2017');
+  t.is(time.format('MM', 1496327490000), '06');
+  t.is(time.format('dd', 1496327490000), '01');
+  t.is(time.format('MMdd', 1496327490000), '0601');
+  t.is(time.format('ruanzhijun', 1496327490000), 'ruanz22ijun');
+});
+
+test('test util.time.str2time()', async (t) => {
+  t.is(time.str2time('2017-06-01 22:31:30'), 1496327490000);
+});
